b53574526a713bacb2b6c0b512c4894f0c0f8147
ewdemy <ewdemy@hotmail.com>
2023-01-24 23:28:10 -0300

    renomeado arquivo db para repository

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
 from fastapi import FastAPI, Depends
 from fastapi_pagination import Page, paginate
 from Utils import Params
-import db
+import repository
 from models import Charada
-from fastapi.openapi.utils import get_openapi
 
 app = FastAPI()
 
-@app.get("/charadas", response_model=Page[db.Charada])
+@app.get("/charadas", response_model=Page[repository.Charada])
 async def buscar_todas_charadas(params: Params = Depends()):
-    return paginate(db.busrcarTodos(), params)
+    return paginate(repository.busrcarTodos(), params)
 
 @app.get("/charadas/charada-aleatoria")
 async def buscar_charada_aleatoria():
-    return db.buscarCharadaAleatoria()
+    return repository.buscarCharadaAleatoria()
 
 @app.get("/charadas/{id}")
 async def buscar_por_id(id: int):
-    return db.buscar_por_id(id)
+    return repository.buscar_por_id(id)
 
 @app.post("/charadas", status_code = 201)
 async def salvar_charada(charada: Charada):
-    return db.salvar_charada(charada)
+    return repository.salvar_charada(charada)
 
 @app.put("/charadas/{id}", status_code = 200)
 async def atualizar_charada(charada: Charada, id: int):
-    return db.atualizar_charada(charada, id)
+    return repository.atualizar_charada(charada, id)
 
 
 
